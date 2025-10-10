8bca890ddca7f739e37dadfeef64cd0950f9e619
Prashanth <prasanthgunti07@gmail.com>
2025-10-10 11:15:23 +0530

    Initial commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,21 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import FunctionComponent from './FunctionComponent'
-import DynamicCode from './DynamicCode'
-import "./DynamicCode.css";
-import ToDo from './ToDo'
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Navbar from "./Components/Navbar.jsx";
+import AddItem from "./Pages/AddItem.jsx";
+import ShowItem from "./Pages/ShowItem.jsx";
+import ItemDetails from "./Pages/ItemDetails.jsx";
+
+import Context, { ContextProvider } from "./Context.jsx"; 
+
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [submitItem, setSubmitItem] = useState([]);
 
-  return (
-    <>
-      <ToDo />
-    </>
-  )
+    return (
+        <ContextProvider>
+            <BrowserRouter>
+                <Navbar />
+                <Routes>
+                    <Route path='/' element={<AddItem />} />
+                    <Route path='/showitem' element={<ShowItem />} />
+                    <Route path='/itemdetails/:id' element={<ItemDetails />} />
+                </Routes>
+            </BrowserRouter>
+        </ContextProvider>
+    );
 }
 
-export default App
-
+export default App;
